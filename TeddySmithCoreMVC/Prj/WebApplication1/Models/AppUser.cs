9bff7d0b5b1ed374e46e6e315cec73db8cd83720
barisTas <baris.tas@teloseng.com>
2023-03-22 10:19:36 +0300

    ilk-commit

---
+++ TeddySmithCoreMVC/Prj/WebApplication1/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication1.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public Address? Address { get; set; }
+        public int? Pace { get; set; }  
+        public  int? Mileage { get; set; }  
+
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+
+    }
+}
+ 
