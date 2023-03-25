cf1063c811791cea24c18b34113903054d6fe297
barisTas <baris.tas@teloseng.com>
2023-03-26 02:41:43 +0300

    models-controllers-views completed

---
+++ TeddySmithCoreMVC/Prj/WebApplication1/Controllers/RaceController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Races.ToList());
+        }
+    }
+}
