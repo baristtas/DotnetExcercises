2794363065fd60544d1740015dc57b16f38e36d1
Baris TAS <baristtas@gmail.com>
2023-04-04 01:15:36 +0300

    edit race completed

--- TeddySmithCoreMVC/Prj/WebApplication1/Controllers/RaceController.cs
+++ TeddySmithCoreMVC/Prj/WebApplication1/Controllers/RaceController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using WebApplication1.Data;
 using WebApplication1.Interfaces;
 using WebApplication1.Models;
-using WebApplication1.Repository;
 using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
@@ -59,6 +56,63 @@ namespace WebApplication1.Controllers
                 m_raceRepository.Add(RaceToAdd);
                 return RedirectToAction("Index");
             }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var ClubToEdit = await m_raceRepository.GetByIdAsync(id);
+            if (ClubToEdit == null) return View("Error");
+
+            var raceVM = new EditRaceViewModel
+            {
+                Title = ClubToEdit.Title,
+                Description = ClubToEdit.Description,
+                AddressId = ClubToEdit.Address.id,
+                Address = ClubToEdit.Address,
+                RaceCategory = ClubToEdit.RaceCategory,
+                URL = ClubToEdit.Image
+            };
+
+            return View(raceVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, EditRaceViewModel raceVMToEdit)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit race");
+                return View("Edit", raceVMToEdit);
+            }
+            var userClub = await m_raceRepository.GetByIdAsync(id);
+
+            if (userClub != null)
+            {
+                try
+                {
+                    await m_photoService.DeletePhotoAsync(userClub.Image);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Couldnt delete previously uploaded photo!");
+                    return View(raceVMToEdit);
+                }
+
+                var newImage = await m_photoService.AddPhotoAsync(raceVMToEdit.Image);
+
+
+                userClub.RaceCategory = raceVMToEdit.RaceCategory;
+                userClub.Address = raceVMToEdit.Address;
+                userClub.AddressId = raceVMToEdit.AddressId;
+                userClub.Image = newImage.Url.ToString();
+                userClub.Title = raceVMToEdit.Title;
+                userClub.Description = raceVMToEdit.Description;
+                userClub.Id = raceVMToEdit.Id;
+
+                m_raceRepository.Update(userClub);
+            }
+
             return RedirectToAction("Index");
         }
     }
